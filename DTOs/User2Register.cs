aaec307f2fdae33fc0d49b0b29069e44d707bbd3
Baph57 <hybridcrayon@yahoo.com>
2020-02-07 16:15:38 -0700

    bla life gets crazy sometimes

--- DTOs/User2Register.cs
+++ DTOs/User2Register.cs
@@ -1,9 +1,15 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace intro2NET.API.DTOs
 {
     public class User2Register
     {
+        [Required]
         public string Username { get; set; }
+
+        [Required]
+        [StringLength(16, MinimumLength = 8, ErrorMessage = "Your password must be at least 8 characters long!")]
         public string Password { get; set; }
     }
 }
